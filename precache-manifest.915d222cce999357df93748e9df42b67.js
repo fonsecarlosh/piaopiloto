3163972bd93101305107b57455ceca800c33fc29
Carlos Fonseca <fonseca.carlosh@gmail.com>
2020-10-08 20:17:06 -0300

    Updates

---
+++ precache-manifest.915d222cce999357df93748e9df42b67.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0d78ee660c9f05a74a6eb1f2938284",
+    "url": "/piaopiloto/index.html"
+  },
+  {
+    "revision": "1669dec7d49b1882b227",
+    "url": "/piaopiloto/static/css/main.65a883dd.chunk.css"
+  },
+  {
+    "revision": "fbe7eb9599a587882e44",
+    "url": "/piaopiloto/static/js/2.bc4c2432.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/piaopiloto/static/js/2.bc4c2432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1669dec7d49b1882b227",
+    "url": "/piaopiloto/static/js/main.97bae5f9.chunk.js"
+  },
+  {
+    "revision": "995b0e932bcf0a166163",
+    "url": "/piaopiloto/static/js/runtime-main.ffb723fc.js"
+  },
+  {
+    "revision": "51b0516a4ad22d1aa3ba9477353e696f",
+    "url": "/piaopiloto/static/media/alessandro.51b0516a.jpg"
+  },
+  {
+    "revision": "6b44561c16619f6368bd3641acb862ee",
+    "url": "/piaopiloto/static/media/alexandre.6b44561c.jpg"
+  },
+  {
+    "revision": "44c8b5e4f8d38d4eba8d12d19de0c955",
+    "url": "/piaopiloto/static/media/arthur.44c8b5e4.jpg"
+  },
+  {
+    "revision": "54780241ec3e87ec43dc3dcfde369a5c",
+    "url": "/piaopiloto/static/media/astronauta.54780241.png"
+  },
+  {
+    "revision": "6bbbf63cddb59a2c0845e9bff5a9c488",
+    "url": "/piaopiloto/static/media/background.6bbbf63c.jpg"
+  },
+  {
+    "revision": "9e4dd557b078991b4cfb8caa5f51a834",
+    "url": "/piaopiloto/static/media/backgroundNave.9e4dd557.jpg"
+  },
+  {
+    "revision": "e343740d7fe0ba8da18fff2ae4a509c2",
+    "url": "/piaopiloto/static/media/bonke.e343740d.jpg"
+  },
+  {
+    "revision": "20bdb211ec0852daed06e84fb497a483",
+    "url": "/piaopiloto/static/media/boop.20bdb211.mp3"
+  },
+  {
+    "revision": "ace5a52d872c7cded7aa6092b707d442",
+    "url": "/piaopiloto/static/media/carol.ace5a52d.jpg"
+  },
+  {
+    "revision": "2fd52d11be4b2e1c990b22be8b89400e",
+    "url": "/piaopiloto/static/media/coletto.2fd52d11.jpg"
+  },
+  {
+    "revision": "4b6072df21baeae8b13363ca06450e78",
+    "url": "/piaopiloto/static/media/diogo.4b6072df.jpg"
+  },
+  {
+    "revision": "025b8a5c87499f000b459ec0b96349ee",
+    "url": "/piaopiloto/static/media/edu.025b8a5c.jpg"
+  },
+  {
+    "revision": "560e9b52b6bf2eaac8f7cdcf4f291c35",
+    "url": "/piaopiloto/static/media/fabricio.560e9b52.jpg"
+  },
+  {
+    "revision": "28acb4a8009c510d6a95d3990164920a",
+    "url": "/piaopiloto/static/media/guilherme.28acb4a8.jpg"
+  },
+  {
+    "revision": "66db18441164aee642ebb8bffc3fc8ed",
+    "url": "/piaopiloto/static/media/guto.66db1844.jpg"
+  },
+  {
+    "revision": "440b9836cb4f9da5e79d533dbd95ae34",
+    "url": "/piaopiloto/static/media/igor.440b9836.jpg"
+  },
+  {
+    "revision": "e87a9bd1c0a4b84cb36ed39d68f9fed8",
+    "url": "/piaopiloto/static/media/liandra.e87a9bd1.jpg"
+  },
+  {
+    "revision": "3eebc8452c335285bae6b0f1ba6c1528",
+    "url": "/piaopiloto/static/media/mariana.3eebc845.jpg"
+  },
+  {
+    "revision": "927debc8ec4ef0b0d529ffcd9646ee7e",
+    "url": "/piaopiloto/static/media/romao.927debc8.jpg"
+  },
+  {
+    "revision": "589d6e562b38e52a2d463f00eee5714f",
+    "url": "/piaopiloto/static/media/siren.589d6e56.mp3"
+  },
+  {
+    "revision": "708b84b8d497e6ec3a183b505feefe01",
+    "url": "/piaopiloto/static/media/tennisball.708b84b8.png"
+  },
+  {
+    "revision": "8a3cfb904afe8d93f48feeac6958ba59",
+    "url": "/piaopiloto/static/media/unkown.8a3cfb90.png"
+  },
+  {
+    "revision": "1b3594ad02d0f257a9faf8a1a3e8f754",
+    "url": "/piaopiloto/static/media/yago.1b3594ad.jpg"
+  }
+]);
